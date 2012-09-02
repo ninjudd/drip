215e21f464204d8aad0c05ff8cdb1a4f0d14f4a3
Justin Balthrop <git@justinbalthrop.com>
2012-09-01 22:08:18 -0700

    add test for scala

--- test/performance.sh
+++ test/performance.sh
@@ -4,6 +4,7 @@ function test_clojure_speed_increase {
     two=$(bench drip -cp clojure.jar clojure.main -e '(* 4 5 6)')
     assert [[ $? == 0 ]]
 
+    # At least 5 times faster.
     assert [[ $(($one / 5)) -gt $two ]]
 }
 
@@ -13,7 +14,18 @@ function test_jruby_speed_increase {
     two=$(bench drip -cp jruby.jar org.jruby.Main -e 'puts 4 * 5 * 6')
     assert [[ $? == 0 ]]
 
+    # At least 5 times faster.
     assert [[ $(($one / 5)) -gt $two ]]
+}
+
+function test_scala_speed_increase {
+    one=$(JAVACMD=drip bench scala/bin/scala -e 'println(1 * 2 * 3)')
+    assert [[ $? == 0 ]]
+    two=$(JAVACMD=drip bench scala/bin/scala -e 'println(4 * 5 * 6)')
+    assert [[ $? == 0 ]]
+
+    # Only slightly faster.
+    assert [[ $(($one * 4)) -gt $(($two * 5)) ]]
 }
 
 function test_runtime_properties {
