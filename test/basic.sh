9dfe47f4688651c53e839fcfeb6ed4f0c98e0a31
Justin Balthrop <git@justinbalthrop.com>
2012-11-20 15:36:03 -0800

    s/ruby/jruby/

--- test/basic.sh
+++ test/basic.sh
@@ -4,7 +4,7 @@ function test_clojure {
     assert_equal 64 "$(drip -cp clojure.jar clojure.main -e '(* 8 8)')"
 }
 
-function test_ruby {
+function test_jruby {
     assert_equal 36 "$(drip -cp jruby.jar org.jruby.Main -e 'puts 6 * 6')"
     assert_equal 49 "$(drip -cp jruby.jar org.jruby.Main -e 'puts 7 * 7')"
     assert_equal 64 "$(drip -cp jruby.jar org.jruby.Main -e 'puts 8 * 8')"
